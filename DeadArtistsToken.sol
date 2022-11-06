f0f07dc235f2d3d103e089757b871b8305de6003
lucca93 <43188541+lucca93@users.noreply.github.com>
2022-11-06 21:49:57 +0100

    Version adjustement

--- DeadArtistsToken.sol
+++ DeadArtistsToken.sol
@@ -5,7 +5,7 @@
 //////////////////////////////////////////
 */
 
-pragma solidity 0.8.4;
+pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
